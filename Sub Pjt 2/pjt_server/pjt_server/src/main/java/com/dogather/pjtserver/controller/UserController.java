0c0c95f7927b55b2f8251c5316091b6543daa84f
Haseok Jang <haseok1020@naver.com>
2022-01-25 16:47:28 +0900

    swagger removed - Haseok Jang

--- Sub Pjt 2/pjt_server/pjt_server/src/main/java/com/dogather/pjtserver/controller/UserController.java
+++ Sub Pjt 2/pjt_server/pjt_server/src/main/java/com/dogather/pjtserver/controller/UserController.java
@@ -100,6 +100,7 @@ public class UserController {
 
 	@GetMapping("/{userId}")
 	public ResponseEntity<UserDto> find(@PathVariable String userId, @RequestHeader String jwt){
+		// 현재는 userId method jwt token 있어야 확인가능 => 본인것만 확인가능
 		System.err.println("(Get)User Controller Find Method run!");
 		//JWT token check
 		String validationResult = JwtProvider.validateToken(jwt, userId);
@@ -120,7 +121,7 @@ public class UserController {
 		JSONObject json = new JSONObject();
 		json.put("result", result);
 		json.put("requested_id", id);
-		return ResponseEntity.status(HttpStatus.OK).body(json.toString()).;//ResponseEntity<UserDto>(userDto, HttpStatus.OK);
+		return ResponseEntity.status(HttpStatus.OK).body(json.toString());//ResponseEntity<UserDto>(userDto, HttpStatus.OK);
 	}
 
 
