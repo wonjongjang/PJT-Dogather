c516aa2a57bbda059b4949f4eb2f509b3b237db8
tpgjs0527 <tpgjs0527@naver.com>
2022-01-27 15:29:23 +0900

    dev-be-group-osh

--- Sub Pjt 2/pjt_server/pjt_server/src/main/java/com/dogather/pjtserver/dao/GroupDao.java
+++ Sub Pjt 2/pjt_server/pjt_server/src/main/java/com/dogather/pjtserver/dao/GroupDao.java
@@ -1,5 +1,15 @@
 package com.dogather.pjtserver.dao;
 
-public interface GroupDao {
+import com.dogather.pjtserver.dto.GroupDto;
+import com.dogather.pjtserver.dto.GroupEnterDto;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
+public interface GroupDao {
+    public int groupRegister(GroupDto groupDto);
+    public int groupUpdate(GroupDto groupDto);
+    public int groupDelete(int groupNo);
+    public GroupDto group(int groupNo);
+    public int groupEnter(GroupEnterDto dto);
+    public int groupOut(GroupEnterDto dto);
 }
--- Sub Pjt 2/pjt_server/pjt_server/src/main/java/com/dogather/pjtserver/dto/GroupDto.java
+++ Sub Pjt 2/pjt_server/pjt_server/src/main/java/com/dogather/pjtserver/dto/GroupDto.java
@@ -1,5 +1,22 @@
 package com.dogather.pjtserver.dto;
 
-public class GroupDto {
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.time.LocalDateTime;
+
+@Data
+@Getter
+@Setter
+public class GroupDto {
+    private int groupNo;
+    private int groupLeader;
+    private LocalDateTime updated;
+    private LocalDateTime created;
+    private LocalDateTime deadline;
+    private int maxPeople;
+    private int end;
+    private int view;
+    private String status;
 }
