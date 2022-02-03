3825988cac1e880bab832277001c6537a4e078ce
HONGJIBEOM <corea1836@naver.com>
2022-02-03 15:41:32 +0900

    FAQ - Main page confilct resolve - jibeom hong

--- Sub Pjt 2/pjt_server/pjt_server/src/main/java/com/dogather/pjtserver/dao/BoardDao.java
+++ Sub Pjt 2/pjt_server/pjt_server/src/main/java/com/dogather/pjtserver/dao/BoardDao.java
@@ -1,5 +1,10 @@
 package com.dogather.pjtserver.dao;
 
+import com.dogather.pjtserver.dto.BoardDto;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
 public interface BoardDao {
 
+    public int createBoard(BoardDto boardDto);
 }
