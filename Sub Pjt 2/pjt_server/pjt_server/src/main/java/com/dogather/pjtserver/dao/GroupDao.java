9e8961c86063dd3747ee23bbe87e918081655aaa
Haseok Jang <haseok1020@naver.com>
2022-02-07 14:16:25 +0900

    User Interest - Haseok Jang

--- Sub Pjt 2/pjt_server/pjt_server/src/main/java/com/dogather/pjtserver/dao/GroupDao.java
+++ Sub Pjt 2/pjt_server/pjt_server/src/main/java/com/dogather/pjtserver/dao/GroupDao.java
@@ -4,6 +4,7 @@ import com.dogather.pjtserver.dto.*;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface GroupDao {
@@ -14,8 +15,8 @@ public interface GroupDao {
     public int groupEnter(GroupEnterDto dto);
     public int groupOut(GroupEnterDto dto);
     public List<GroupReturnDto> getList();
-    public int interest(GroupInterestDto dto);
+    public int addInterest(GroupInterestDto dto);
     public List<GroupInterestDto> interestlist(int userNo);
-    public List<GroupInterestDto> interestlistdetail(List<GroupInterestDto> dto);
     public List<OptionDto> getOptions(int groupNo);
+    public void addOption(Map<String, Object> param);
 }
