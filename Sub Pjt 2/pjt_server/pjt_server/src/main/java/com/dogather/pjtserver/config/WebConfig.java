8e17ca9959356f1c266f13970d73a1293439563f
Haseok Jang <haseok1020@naver.com>
2022-02-09 14:29:04 +0900

    intercepter - Haseok Jang

--- Sub Pjt 2/pjt_server/pjt_server/src/main/java/com/dogather/pjtserver/config/WebConfig.java
+++ Sub Pjt 2/pjt_server/pjt_server/src/main/java/com/dogather/pjtserver/config/WebConfig.java
@@ -22,7 +22,8 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addInterceptor(new LoginInterceptor())
                 .addPathPatterns("/**")
                 .excludePathPatterns(
-                        "",
+                        "/",
+                        "/*",
                         "/user/register",
                         "/user/login",
                         "/user/idcheck",
