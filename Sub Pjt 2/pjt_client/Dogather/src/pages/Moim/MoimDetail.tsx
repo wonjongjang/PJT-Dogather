82287fa560dbf773eb69d676a98670dbe190e705
Yoon Gwan <blackgar@naver.com>
2022-02-13 14:47:48 +0900

    upload kakaopay - YG

--- Sub Pjt 2/pjt_client/Dogather/src/pages/Moim/MoimDetail.tsx
+++ Sub Pjt 2/pjt_client/Dogather/src/pages/Moim/MoimDetail.tsx
@@ -8,7 +8,7 @@ import {
   useParams,
 } from "react-router-dom";
 import styled from "styled-components";
-import { CardMedia, Stack } from "@mui/material";
+import { CardMedia, Input, Stack } from "@mui/material";
 import { FetchMoimGroupAPI, FetchMoimMediaAPI } from "../../api/MoimDetail";
 // import { Audio, Hearts } from "react-loader-spinner";
 import { Link } from "react-router-dom";
@@ -20,6 +20,7 @@ import { useRecoilValue } from "recoil";
 import { userIdAtom } from "../../atoms/Login";
 import Hoodie from "../../img/Hoodie.png";
 import MoimDetailImg from "./MoimDetailComponent/MoimDetailImg";
+import KakaoPay from "./KakaoPay";
 
 interface RouteState {
   state: {
@@ -56,7 +57,6 @@ export interface IGroupData {
   mediaList: Array<string>;
   faqList: Array<object>;
 }
-
 function MoimDetail() {
   // useEffect(() => {
   //   (async () => {
@@ -109,20 +109,71 @@ function MoimDetail() {
     }, 100);
   }, []);
 
-  const [optionValue, setOptionValue] = useState("");
-  const [quantity, setQuantity] = useState("");
+  const basePrice = groupData?.price;
+  const [options, setOption] = React.useState<string[]>([]);
+  const [sum, setSum] = React.useState(0);
+
+  const [sumPrice, setSumPrice] = React.useState(0);
+  const [productOption, setProductOption] = useState(groupData?.options);
+  const [optionIdx, setOptionIdx] = useState(0);
+  const [optionPrice, setOprionPrice] = useState(0);
+  const [quantity, setQuantity] = useState(1);
   const [isHidden, setIsHidden] = useState(true);
+  const [optionHidden, setOptionHidden] = useState(true);
+  const [price, setPrice] = useState(groupData?.price);
+  console.log(price);
+
+  // const selectOption = (e: React.ChangeEvent<HTMLSelectElement>) => {
+  //   const _options = options.filter((o, idx) => {
+  //     if (o !== e.target.value) {
+  //       return o;
+  //     }
+  //   });
+  //   let price = Number(e.target.value.slice(-7, -1).split(",").join("").trim());
+  //   setOption([
+  //     ..._options,
+  //     { text: e.target.value, num: 1, prd: prd, sum: price },
+  //   ]);
+  //   setSum(sum + price);
+  //   setHidden(false);
+  //   setOptionIdx(Number(e.target.value));
+  // };
+
   // console.log(options);
   // console.log(options[0].optionName);
   const onSelect = (event: React.ChangeEvent<HTMLSelectElement>) => {
     console.log(event.target.value);
+    const value = event.target.value;
+    // console.log(typeof event.target.value);
+    const _options: string[] = options.filter((i) => {
+      if (i !== event.target.value) {
+        return i;
+      }
+    });
+    // const _quantitys: string[] = quantity.filter((q) => {
+    //   if ()
+    // })
+    setOption([..._options, value]);
+    // setQuantity([])
+    // setPrice([])
     setIsHidden(false);
-    setOptionValue(event.target.value);
+    setOptionIdx(Number(event.target.value));
   };
-  const onQuantity = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setQuantity(event.target.value);
-  };
+  //처음 랜더링되고 유저에게 보이는 수량의 값이 0일 필요가 없기 때문에 초기값으로 1로 주었다.
 
+  const increaseQuantity = () => {
+    setQuantity((prevCount) => prevCount + 1);
+    setPrice(() => price! + basePrice!);
+  };
+  const decreaseQuantity = () => {
+    setQuantity((prevCount) => prevCount - 1);
+    setPrice(() => price! - basePrice!);
+  };
+  const handleValue = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = Number(e.target.value);
+    setQuantity(Number(value));
+    console.log(value);
+  };
   const mainImgAddress = "/doimage/" + groupData?.mainImage;
   console.log(groupData?.mainImage);
   const detailImgAddress = "/doimage/" + groupData?.mediaList[0];
@@ -130,6 +181,8 @@ function MoimDetail() {
 
   const time = Date.now();
   console.log(time);
+  const makeComma = (price: number) =>
+    price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 
   return (
     <Container>
@@ -172,6 +225,14 @@ function MoimDetail() {
                 </Link>
                 <ProductTitle>{groupData?.product}</ProductTitle>
                 <ProductDetail>{groupData?.detail}</ProductDetail>
+                <ProductPrice>
+                  <ProductOriginalPrice>
+                    {makeComma(groupData?.originPrice!) + "원"}
+                  </ProductOriginalPrice>
+                  <ProductMoimPrice>
+                    {makeComma(groupData?.price!) + "원"}
+                  </ProductMoimPrice>
+                </ProductPrice>
                 <Option>{"옵션선택"}</Option>
                 {/* {groupData?.options.map((option, idx) => (
                   <MoimSelect
@@ -191,43 +252,79 @@ function MoimDetail() {
                         </SelectOption>
                         {groupData?.options.map((option, idx) => (
                           <SelectOption
-                            key={idx}
-                            value={
-                              option.optionName +
-                              " (" +
-                              "+" +
-                              option.optionPrice +
-                              ")"
-                            }
+                            key={option.optionName}
+                            // onChange={}
+                            value={idx}
                           >
                             {option.optionName}
                           </SelectOption>
                         ))}
                       </SelectSelect>
                     </SelectContent>
-                    <OptionWrapper hidden={isHidden}>
-                      <SelectContent>
-                        <SelectInput
-                          value={quantity}
-                          onChange={onQuantity}
-                          type="number"
-                          min="0"
-                          placeholder="수량을 입력해주세요."
-                        />
-                      </SelectContent>
-                      <SelectContent>
-                        <span>옵션명 : {optionValue}</span>
-                        <br />
-                        <span>수량 : {quantity}</span>
-                        <br />
-                      </SelectContent>
-                      <SelectContent>
-                        <Stack direction="row" spacing={2}>
-                          <Button>장바구니</Button>
-                          <Button>바로구매</Button>
-                        </Stack>
-                      </SelectContent>
+                    {options.map((o, idx) => {
+                      return (
+                        <>
+                          <OptionWrapper key={idx}>
+                            <CartOption>
+                              <SelectContentItem>
+                                <OptionName>
+                                  옵션명 :{" "}
+                                  {groupData?.options[Number(o)].optionName +
+                                    "(+" +
+                                    groupData?.options[Number(o)].optionPrice +
+                                    ")"}
+                                </OptionName>
+                              </SelectContentItem>
+                              <SelectContentItem>
+                                <QuantityButton
+                                  className="purchaseButton"
+                                  onClick={decreaseQuantity} //onClick이 되면 카운팅이 감소되는 함수실행
+                                  disabled={quantity < 1}
+                                >
+                                  {"-"}
+                                </QuantityButton>
+                                <SelectInput
+                                  className="productQuantity"
+                                  type="text"
+                                  onChange={handleValue} //onChange될때마다 값을 얻음
+                                  value={quantity} //값은 count의 state를 담는다.
+                                />
+                                <QuantityButton
+                                  className="purchaseButton"
+                                  onClick={increaseQuantity}
+                                >
+                                  {"+"}
+                                </QuantityButton>
+                              </SelectContentItem>
+                            </CartOption>
+                          </OptionWrapper>
+                        </>
+                      );
+                    })}
+                    <OptionWrapper>
+                      <CartOption>
+                        <PriceWrapper>
+                          <ProductQuantity>
+                            {"총 " + quantity + "개"}
+                          </ProductQuantity>
+                          <FinalPrice>
+                            {makeComma(
+                              (basePrice! +
+                                Number(
+                                  groupData?.options[optionIdx].optionPrice
+                                )) *
+                                quantity
+                            ) + "원"}
+                          </FinalPrice>
+                        </PriceWrapper>
+                      </CartOption>
                     </OptionWrapper>
+                    <SelectContent>
+                      <Button style={{ backgroundColor: "tomato" }}>
+                        관심등록
+                      </Button>
+                      <KakaoPay />
+                    </SelectContent>
                   </SelectWrapper>
                 </SelectContainer>
 
@@ -287,7 +384,9 @@ const ProductTitle = styled.p`
   margin-bottom: 10px;
 `;
 const ProductDetail = styled.p`
-  margin-bottom: 20px;
+  margin-bottom: 10px;
+  word-spacing: 2px;
+  line-height: 20px;
 `;
 
 const Option = styled.p`
@@ -296,7 +395,20 @@ const Option = styled.p`
   margin-bottom: 5px;
 `;
 
-const ProductPrice = styled.p``;
+const ProductPrice = styled.p`
+  margin-bottom: 20px;
+`;
+const ProductOriginalPrice = styled.p`
+  color: grey;
+  font-size: 16px;
+  /* margin-bottom: 2px; */
+
+  text-decoration: line-through;
+`;
+const ProductMoimPrice = styled.p`
+  font-size: 24px;
+  font-weight: bold;
+`;
 const Container = styled.div`
   display: flex;
   justify-content: center;
@@ -335,6 +447,7 @@ const Overview = styled.div`
   justify-content: space-evenly;
   background-color: white;
   padding: 10px 20px;
+
   /* box-shadow: 3px 3px 10px 3px lightgrey; */
 `;
 
@@ -344,7 +457,7 @@ const OverviewItem = styled.div`
   justify-content: center;
   align-items: left;
   width: 600px;
-  height: 500px;
+  height: auto;
   span {
     font-size: 20px;
     font-weight: 400;
@@ -381,35 +494,55 @@ const SelectContainer = styled.div`
   display: block;
 `;
 
-const OptionWrapper = styled.div``;
+const OptionWrapper = styled.div`
+  border-color: grey;
+  border-width: 1px;
+`;
 
 const SelectContent = styled.div`
   display: flex;
   justify-content: right;
   font-size: 20px;
+  width: 100%;
+`;
+const SelectContentItem = styled.div`
+  display: flex;
+  justify-content: left;
+  width: 100%;
 `;
 
 const SelectCart = styled.button`
-  display: flex;
+  display: block;
   justify-content: center;
   font-size: 20px;
 `;
 
 const SelectWrapper = styled.div``;
 
-const SelectInput = styled.input`
-  font-size: 14px;
-  width: 100%;
+const QuantityButton = styled.button`
+  width: 30px;
   height: 30px;
-  margin-bottom: 10px;
+  background-color: white;
+  border-width: 1px;
+  border-color: grey;
+  :hover {
+    background-color: lightgrey;
+    opacity: 0.5;
+  }
+`;
+
+const SelectInput = styled.input`
+  font-size: 16px;
   color: grey;
+  width: 50px;
+  height: 30px;
+  text-align: center;
+  margin-bottom: 10px;
+  /* border-radius: 5px; */
+  border-color: black;
+  border-width: 1px;
   :hover {
     outline: auto;
-  }
-  ::-webkit-outer-spin-button {
-    width: 50px;
-    height: 20px;
-    -webkit-appearance: "Always Show Up/Down Arrows";
   }
   ::-webkit-inner-spin-button {
     width: 50px;
@@ -420,12 +553,14 @@ const SelectInput = styled.input`
 
 const SelectSelect = styled.select`
   width: 100%;
-  height: 30px;
-  font-size: 14px;
+  height: 40px;
+  font-size: 16px;
   color: grey;
-  margin-bottom: 3px;
+  margin-bottom: 10px;
+  border-radius: 5px;
+  border-color: black;
+  border-width: 2px;
   :hover {
-    /* background-color: #e2e0ff; */
     outline: auto;
   }
 `;
@@ -434,6 +569,38 @@ const SelectOption = styled.option`
   font-size: 14px;
 `;
 
-const Button = styled.button``;
+const CartOption = styled.div`
+  border-color: grey;
+`;
+
+const OptionName = styled.p`
+  font-size: 16px;
+  margin-bottom: 5px;
+`;
+
+const PriceWrapper = styled.p`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const ProductQuantity = styled.p`
+  font-size: 14px;
+`;
+const FinalPrice = styled.p`
+  font-size: 20px;
+  font-weight: bold;
+`;
+
+const Button = styled.button`
+  width: 100%;
+  height: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 5px;
+  border-radius: 10px;
+  font-weight: bold;
+  color: white;
+`;
 
 export default MoimDetail;
